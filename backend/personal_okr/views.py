2823de967a3e130a1e04fa9605e46117ece3a811
Vu Hai Ninh <haininh.vu89@gmail.com>
2019-12-03 18:44:32 +0700

    update flake8

--- backend/personal_okr/views.py
+++ backend/personal_okr/views.py
@@ -6,5 +6,8 @@ from django.http import JsonResponse
 
 def char_count(request):
     text = request.GET.get("text", "")
-    return JsonResponse({"count": len(text)}) 
-    
+    return JsonResponse({"count": len(text)})
+
+
+def add(x, y):
+    return x + y
