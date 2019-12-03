b95b3854a5699a2488868b79fca475c1d8b4f820
Vu Hai Ninh <haininh.vu89@gmail.com>
2019-12-03 18:12:14 +0700

    blank line

--- backend/personal_okr/views.py
+++ backend/personal_okr/views.py
@@ -1,8 +1,9 @@
-#from django.shortcuts import render
+# from django.shortcuts import render
 from django.http import JsonResponse
 
 # Create your views here.
+
+
 def char_count(request):
     text = request.GET.get("text", "")
     return JsonResponse({"count": len(text)})
-    
