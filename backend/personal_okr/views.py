1fb95347145c59ee496221f050c78119c1222e05
Vu Hai Ninh <haininh.vu89@gmail.com>
2019-12-03 14:09:16 +0700

    create personal okr

---
+++ backend/personal_okr/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+# Create your views here.
+def char_count(request):
+    text = request.GET.get("text", "")
+
+    return JsonResponse({"count": len(text)})
