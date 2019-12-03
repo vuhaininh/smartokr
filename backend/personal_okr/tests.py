09858d8ace83e50afef7a75d196d5757cd47bd7f
Vu Hai Ninh <haininh.vu89@gmail.com>
2019-12-03 19:14:25 +0700

    unit test

--- backend/personal_okr/tests.py
+++ backend/personal_okr/tests.py
@@ -1,3 +1,10 @@
-# from django.test import TestCase
-
+from django.test import TestCase
+from personal_okr.views import add
 # Create your tests here.
+
+
+class ViewsTests(TestCase):
+
+    def test_add_numbers(self):
+        """"Test that 2 numbers added together"""
+        self.assertEqual(add(3, 8), 11)
