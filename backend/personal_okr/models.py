d251a9583fa254076009d668ed5ba0df9b04fabe
Vu Hai Ninh <haininh.vu89@gmail.com>
2019-12-06 22:15:11 +0700

    Create User, Token API

--- backend/personal_okr/models.py
+++ backend/personal_okr/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+# from django.conf import settings
 
 
 class Objective(models.Model):
+
     description = models.CharField(max_length=1000)
     finished_date = models.DateField()
---
+++ backend/users/tests/test_users_api.py
@@ -0,0 +1,99 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework.test import APIClient
+
+from rest_framework import status
+
+CREATE_USER_URL = reverse('users:create')
+TOKEN_URL = reverse('users:token')
+
+
+def create_user(**params):
+    return get_user_model().objects.create_user(**params)
+
+
+class PublicUserApiTests(TestCase):
+    """Test the users API (public)"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_valid_user_success(self):
+        """Test creating user with valid payload is successful"""
+        payload = {
+            'email': 'haininh@test.com',
+            'password': 'testpassword1',
+            'first_name': 'Mike',
+            'last_name': 'Jordon'
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(**res.data)
+        self.assertTrue(user.check_password(payload["password"]))
+        self.assertNotIn('password', res.data)
+
+    def test_user_exists(self):
+        """Test creating user that already exists fails"""
+        payload = {
+            'email': 'haininh@test.com',
+            'password': 'testpassword1',
+            'first_name': 'Mike',
+            'last_name': 'Jordon'
+        }
+        create_user(**payload)
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_password_too_short(self):
+        """Test that password must be more than 5 characters"""
+        payload = {
+            'email': 'haininh@test.com',
+            'password': 'te',
+            'first_name': 'Mike',
+            'last_name': 'Jordon'
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']
+        ).exists()
+        self.assertFalse(user_exists)
+
+    def test_create_token_for_user(self):
+        """Test that token is created for user"""
+        payload = {
+            'email': 'haininh@test.com',
+            'password': 'testpassword1',
+        }
+        create_user(**payload)
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_invalid_credentials(self):
+        """Test that token is not created if invalid credentials are given"""
+        create_user(email='haininh@test.com', password='rightpassword')
+        payload = {
+            'email': 'haininh@test.com',
+            'password': 'testpassword1',
+        }
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_no_user(self):
+        """Test that token is not created if user doesn't exist"""
+        payload = {
+            'email': 'haininh@test.com',
+            'password': 'testpassword1',
+        }
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_missing_field(self):
+        """Test that email and password are required"""
+        res = self.client.post(TOKEN_URL, {'email': 'one', 'password': ' '})
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code,  status.HTTP_400_BAD_REQUEST)
