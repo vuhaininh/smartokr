6a8de35a2d1e1dada67315b9d541dfceef6e5a6b
Vu Hai Ninh <haininh.vu89@gmail.com>
2019-12-07 14:18:50 +0700

    Modify User API

--- backend/users/tests/test_users_api.py
+++ backend/users/tests/test_users_api.py
@@ -2,7 +2,6 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework.test import APIClient
-
 from rest_framework import status
 
 CREATE_USER_URL = reverse('users:create')
@@ -115,8 +114,31 @@ class PrivateUserApiTests(TestCase):
             first_name='Jane',
             last_name='Kitty'
         )
-
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
-    
-    
+
+    def test_retrieve_profile_success(self):
+        """Test retrieving profile for logged in used"""
+        res = self.client.get(ME_URL)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+            'first_name': self.user.first_name,
+            'last_name': self.user.last_name,
+            'email': self.user.email
+        })
+
+    def test_post_me_not_allowed(self):
+        """Test that POST is not allowed on the me url"""
+        res = self.client.post(ME_URL, {})
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        """Test updating the user profile for authenticated user"""
+        payload = {'first_name': 'John', 'last_name': 'Lennon',
+                   'password': 'anotherpassword'}
+        res = self.client.patch(ME_URL, payload)
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.first_name, payload['first_name'])
+        self.assertEqual(self.user.last_name, payload['last_name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
